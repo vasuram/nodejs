2cef698e5254a49405783002c7a2300d76be9d99
vasuram <vasu_ram@yahoo.com>
2019-09-30 18:39:11 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,15 @@
-echo "# nodejs" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin git@github.com:vasuram/nodejs.git
-git push -u origin master
+ var http = require("http")
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'})
+
+   // Send the response body as "Hello World"
+   response.end('Hello to megajala\n')
+}).listen(3000)
+
+// Console will print the message
+console.log('Server running')
